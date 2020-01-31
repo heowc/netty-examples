68acfa46690539fae253d4178c9e38e8b53b52d8
heowc <heowc1992@gmail.com>
2020-01-31 22:17:42 +0900

    Add executor-group

---
+++ executor-group/src/main/java/com/example/Main.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+
+import java.nio.charset.Charset;
+
+public class Main {
+
+    private final int port;
+
+    public Main(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        final ByteBuf buf = Unpooled.copiedBuffer("Hi!\r\n", Charset.defaultCharset());
+
+        final DefaultEventExecutorGroup executorGroup = new DefaultEventExecutorGroup(10);
+        final EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(port)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(executorGroup, new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) {
+                                    // #eventLoopGroup
+                                    // Thread[nioEventLoopGroup-3-2,10,main]
+                                    // io.netty.channel.nio.NioEventLoop@f0f2775
+
+                                    // #executorGroup
+                                    // Thread[defaultEventExecutorGroup-2-1,5,main]
+                                    // io.netty.util.concurrent.DefaultEventExecutor@1e4a7dd4
+                                    System.out.println(Thread.currentThread());
+                                    System.out.println(ctx.executor());
+                                    ctx.writeAndFlush(buf.duplicate())
+                                        .addListener(ChannelFutureListener.CLOSE);
+                                }
+                            });
+                        }
+                    });
+
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        new Main(port).run();
+    }
+}
